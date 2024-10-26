128b3193e970acc5eed2fb4b285bfd7dcfe75f8c
Victor Kipkurui <kipkurui.victor18@students.dkut.ac.ke>
2024-10-26 14:03:16 +0300

    post

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import { Outlet } from 'react-router-dom'
-
-
+import './index.css'
 const App = () => {
   return (
     <div>
